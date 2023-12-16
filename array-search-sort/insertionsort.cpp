bd188537b434546822cce6baee7c9003d5e27231
vinay-nmamit <vk5022702@gmail.com>
2023-12-16 11:36:32 +0530

    second

--- array-search-sort/insertionsort.cpp
+++ array-search-sort/insertionsort.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 int main() {
     int n;
+    cout << "Enter the Size of the array: ";
     cin >> n;
 
     int arr[n];
+    cout << "Enter the elements into the array: ";
     for(int i=0; i<n; i++){
         cin >> arr[i];
     }
@@ -20,6 +22,7 @@ int main() {
         arr[j+1] = current;
     }
 
+    cout << "Sorted array: ";
     for(int i = 0; i < n; i++){
         cout << arr[i] << " ";
     }
